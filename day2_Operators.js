c7f829f19a4db4971e859c6f58368a732e26001c
triggereddown <deepmoitra2@gmail.com>
2024-07-14 21:55:23 +0530

    Day 2 Practice

---
+++ day2_Operators.js
@@ -0,0 +1,40 @@
+//Arithmatic operations
+let a=20;
+let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Assignment Operators
+let c=30;
+c+=10
+console.log(c);
+c-=10;
+console.log(c);
+
+//Comparison Operators
+console.log(10>5);
+console.log(10<5);
+console.log(10>=10);
+console.log(10==10);
+let d=10;
+var e=20;
+console.log(d==e);
+console.log(d===e);
+
+//Logical Operators
+console.log(10>3 && 10<4);
+console.log(10>4 || 10<5);
+console.log(10!=9);
+
+//Ternary Operators
+let res= (-2>0)? true:false;
+if(res==true)
+{
+    console.log("Positive")
+}
+else{
+    console.log("Negetive")
+}
